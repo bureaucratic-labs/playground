4b196f98fd90edd9ad0c45473201561f4afe2ca7
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-09-17 17:31:39 +0300

    Add loader.io verification code

--- main.py
+++ main.py
@@ -22,9 +22,12 @@ async def extract(request):
     results = app['combinator'].extract(form['text'])
     return web.json_response(list(serialize(results)), dumps=json_dumps)
 
+def loader_verification(request):
+    return web.Response(body=b'loaderio-f68cd84a6bf29dc6f5b6fe0726bce603')
 
 def init(argv):
     app = web.Application()
-    app.router.add_post("/api/extract", extract)
+    app.router.add_post('/api/extract', extract)
+    app.router.add_get('/loaderio-f68cd84a6bf29dc6f5b6fe0726bce603.txt', loader_verification)
     app['combinator'] = Combinator(DEFAULT_GRAMMARS)
     return app
