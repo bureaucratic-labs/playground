a8c8897ce341faef83e9b5a8dc8687582d59fa75
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-09-18 18:33:48 +0300

    Add CORS support

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import logging
-
 from ujson import dumps
-from aiohttp import web
 from functools import partial
+
+from aiohttp import web
+import aiohttp_cors
 
 from natasha import Combinator, DEFAULT_GRAMMARS
 
-
-logging.basicConfig(level=logging.INFO)
 json_dumps = partial(dumps, ensure_ascii=False)
 
 
@@ -24,12 +22,15 @@ async def extract(request):
     results = request.app['combinator'].extract(form['text'])
     return web.json_response(list(serialize(results)), dumps=json_dumps)
 
-def loader_verification(request):
-    return web.Response(body=b'loaderio-f68cd84a6bf29dc6f5b6fe0726bce603')
-
 def init(argv):
     app = web.Application()
-    app.router.add_post('/api/extract', extract)
-    app.router.add_get('/loaderio-f68cd84a6bf29dc6f5b6fe0726bce603.txt', loader_verification)
     app['combinator'] = Combinator(DEFAULT_GRAMMARS)
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+            )
+    })
+    cors.add(app.router.add_route('POST', '/api/extract', extract))
     return app
